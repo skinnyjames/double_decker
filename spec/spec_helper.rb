4b1650837ab3a36781e1ba5a18fc11d43310a9b1
Sean Gregory <sean.christopher.gregory@gmail.com>
2021-04-03 07:37:42 -0400

    fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'knapsack'
 
 Knapsack::Adapters::RSpecAdapter.bind
 
-bus = DoubleDecker::Bus.new SecureRandom.uuid, expected_agents: 2
+bus = DoubleDecker::Bus.new 'test-run', expected_agents: ENV['KNAPSACK_PRO_CI_NODE_TOTAL']
 
 bus.on_finished do |payload|
   File.open("result.json", "w") do |f|
