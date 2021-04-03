54d088074efdb184f0ea073ad9375abd12b08cb3
Sean Gregory <sean.christopher.gregory@gmail.com>
2021-04-03 08:08:58 -0400

    fix

--- post_spec/final_spec.rb
+++ post_spec/final_spec.rb
@@ -1,6 +1,6 @@
 describe "Output" do 
   it "should have 2 agents" do 
-    data = JSON.parse File.read("#{__dir__}/../report.json")
+    data = JSON.parse File.read("result.json")
     expect(data.keys.length).to eql(2)
   end
 end
